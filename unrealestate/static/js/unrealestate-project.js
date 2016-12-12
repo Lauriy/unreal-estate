fd0c452be2fac216c3b00b43a1dd0b49550de049
Lauri Elias <laurileet@gmail.com>
2016-12-12 22:51:59 +0200

    Improve project detail page, remove some static crap, fix logo

--- unrealestate/static/js/unrealestate-project.js
+++ unrealestate/static/js/unrealestate-project.js
@@ -3,12 +3,27 @@
     /*jslint nomen: true*/
     /*jslint browser: true*/
     $(document).ready(function () {
-        $('#slider').slidertron({
-            viewerSelector: '.viewer',
-            reelSelector: '.viewer .reel',
-            slidesSelector: '.viewer .reel .slide',
-            advanceDelay: 3000,
-            speed: 'slow'
-        });
+        var $sliders = $('.slider');
+        if ($sliders.length > 0) {
+            $sliders.slidertron({
+                mode: 'fadeIn',
+                seamlessWrap: true,
+                viewerSelector: '.viewer',
+                reelSelector: '.viewer .reel',
+                slidesSelector: '.viewer .reel .slide',
+                advanceDelay: 0,
+                speed: 400,
+                fadeInSpeed: 1000,
+                autoFit: true,
+                autoFitAspectRatio: (840 / 344),
+                navPreviousSelector: '.nav-previous',
+                navNextSelector: '.nav-next',
+                indicatorSelector: '.indicator ul li',
+                slideLinkSelector: '.link'
+            });
+            $(window).on('resize load', function () {
+                $sliders.trigger('slidertron_reFit');
+            }).trigger('resize');
+        }
     });
 }(jQuery));
